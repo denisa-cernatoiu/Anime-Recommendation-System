5117c5e419ed050fc442800c823f313156bb10fc
denisa-cernatoiu <denisacernatoiu@yahoo.com>
2026-02-19 23:28:36 +0200

    Fix import path for AnimeRecommendation class

--- recommender.py
+++ recommender.py
@@ -78,7 +78,7 @@ if __name__ == "__main__":
     df = pd.read_csv("anime_dataset_cleaned.csv")
     train_df = pd.DataFrame(df, columns=['title_english', 'description'])
     
-    rec = AnimeRecommendation()
+    rec = recommender.AnimeRecommendation()
     rec.fit(train_df,title_col='title_english', description_col='description')
     rec.save()
     print('Model trained!')
